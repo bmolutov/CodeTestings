c6a1dbf58a55ab39ef29fa1fd83c41e529ca881b
Bekzat Molutov <b_molutov@kbtu.kz>
2022-03-24 18:30:23 +0600

    new

--- pygame/pixelRunner/main.py
+++ pygame/pixelRunner/main.py
@@ -31,6 +31,18 @@ def collisions(player, obstacles):
                 return False
     return True
 
+def player_animation():
+    # play walking animation if the player is on floor
+    # display the jump surface when player is not on floor
+    global player_surf, player_index
+    if player_rect.bottom < 300:
+        player_surf = player_jump
+    else:
+        player_index += 0.1
+        if (player_index >= len(player_walk)):
+            player_index = 0
+        player_surf = player_walk[int(player_index)]
+
 # starts pygame, and helps run our project
 pygame.init()
 
@@ -53,12 +65,30 @@ sky_surf = pygame.image.load('../assets/graphics/Sky.png').convert() # convert()
 ground_surf = pygame.image.load('../assets/graphics/ground.png').convert()
 
 # Obstacles
-snail_surf = pygame.image.load('../assets/graphics/snail/snail1.png').convert_alpha()
-fly_surf = pygame.image.load('../assets/graphics/Fly/Fly1.png').convert_alpha()
+# Snail
+snail_frame_1 = pygame.image.load('../assets/graphics/snail/snail1.png').convert_alpha()
+snail_frame_2 = pygame.image.load('../assets/graphics/snail/snail2.png').convert_alpha()
+snail_frames = [snail_frame_1, snail_frame_2]
+snail_frame_index = 0
+snail_surf = snail_frames[snail_frame_index]
+
+# Fly
+fly_frame_1 = pygame.image.load('../assets/graphics/Fly/Fly1.png').convert_alpha()
+fly_frame_2 = pygame.image.load('../assets/graphics/Fly/Fly2.png').convert_alpha()
+fly_frames = [fly_frame_1, fly_frame_2]
+fly_frame_index = 0
+fly_surf = fly_frames[fly_frame_index]
 
 obstacle_rect_list = []
 
-player_surf = pygame.image.load('../assets/graphics/Player/player_walk_1.png').convert_alpha()
+# Player
+player_walk_1 = pygame.image.load('../assets/graphics/Player/player_walk_1.png').convert_alpha()
+player_walk_2 = pygame.image.load('../assets/graphics/Player/player_walk_2.png').convert_alpha()
+player_walk = [player_walk_1, player_walk_2]
+player_index = 0
+player_jump = pygame.image.load('../assets/graphics/Player/jump.png').convert_alpha()
+
+player_surf = player_walk[player_index]
 player_rect = player_surf.get_rect(midbottom=(80, 300)) # it draws a rectangle around this surface
 
 player_gravity = 0
@@ -77,6 +107,12 @@ game_message_rect = game_message.get_rect(center = (400, 340))
 # Timer
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 1500)
+
+snail_animation_timer = pygame.USEREVENT + 2
+pygame.time.set_timer(snail_animation_timer, 500)
+
+fly_animation_timer = pygame.USEREVENT + 3
+pygame.time.set_timer(fly_animation_timer, 200)
 
 while True:
     # draw all our elements
@@ -99,22 +135,29 @@ while True:
                 game_active = True
                 start_time = pygame.time.get_ticks()
         
-        if event.type == obstacle_timer and game_active:
-            if randint(0, 2):
-                obstacle_rect_list.append(snail_surf.get_rect(bottomright=(randint(900, 1100), 300)))
-            else:
-                obstacle_rect_list.append(fly_surf.get_rect(bottomright=(randint(900, 1100), 210)))
+        if game_active:
+            if event.type == obstacle_timer:
+                if randint(0, 2):
+                    obstacle_rect_list.append(snail_surf.get_rect(bottomright=(randint(900, 1100), 300)))
+                else:
+                    obstacle_rect_list.append(fly_surf.get_rect(bottomright=(randint(900, 1100), 210)))
+            if event.type == snail_animation_timer:
+                snail_frame_index = 1 if snail_frame_index == 0 else 0
+                snail_surf = snail_frames[snail_frame_index]
+            if event.type == fly_animation_timer:
+                fly_frame_index = 1 if fly_frame_index == 0 else 0
+                fly_surf = fly_frames[fly_frame_index]
 
     if game_active:
         screen.blit(sky_surf, (0, 0)) # drawing the surface at the topside of its parent
         screen.blit(ground_surf, (0, 300))
-        
         score = display_score()
 
         # Player
         player_gravity += 1
         player_rect.y += player_gravity
         if player_rect.bottom >= 300: player_rect.bottom = 300
+        player_animation()
         screen.blit(player_surf, player_rect) # we are taking a surface and placing it on the position of rectangle
 
         # Obstacle movement
